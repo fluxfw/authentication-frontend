06b445fab06777283fa37354e82009d402217f19
fluxfw <fluxfw@fluxlabs.ch>
2023-03-27 15:41:13 +0200

    `flux-overlay`

--- src/Authentication/ShowAuthentication.mjs
+++ src/Authentication/ShowAuthentication.mjs
@@ -1,3 +1,5 @@
+import { AUTHENTICATION_LOCALIZATION_MODULE } from "../Localization/_LOCALIZATION_MODULE.mjs";
+
 /** @typedef {import("./_authenticate.mjs").authenticate} _authenticate */
 /** @typedef {import("../../../flux-localization-api/src/FluxLocalizationApi.mjs").FluxLocalizationApi} FluxLocalizationApi */
 /** @typedef {import("./setHideAuthentication.mjs").setHideAuthentication} setHideAuthentication */
@@ -33,31 +35,85 @@ export class ShowAuthentication {
      * @returns {Promise<void>}
      */
     async showAuthentication(authenticate, set_hide_authentication, switch_to_offline_mode = null) {
-        const { AuthenticationElement } = await import("./AuthenticationElement.mjs");
+        let resolve_promise;
 
-        await new Promise(resolve => {
-            const authentication_element = AuthenticationElement.new(
-                this.#flux_localization_api,
-                () => {
-                    authenticate();
-                },
-                switch_to_offline_mode !== null ? () => {
-                    switch_to_offline_mode();
-                } : null
-            );
-
-            document.body.appendChild(authentication_element);
-
-            set_hide_authentication(
-                () => {
-                    authentication_element.reset();
-                },
-                () => {
-                    authentication_element.remove();
-
-                    resolve();
-                }
-            );
+        const promise = new Promise(resolve => {
+            resolve_promise = resolve;
         });
+
+        const {
+            FLUX_OVERLAY_BUTTON_CLICK_EVENT,
+            FluxOverlayElement
+        } = await import("../../../flux-overlay/src/FluxOverlayElement.mjs");
+
+        const flux_overlay_element = FluxOverlayElement.new(
+            await this.#flux_localization_api.translate(
+                "Authentication required",
+                AUTHENTICATION_LOCALIZATION_MODULE
+            ),
+            null,
+            [
+                {
+                    label: await this.#flux_localization_api.translate(
+                        "Authenticate",
+                        AUTHENTICATION_LOCALIZATION_MODULE
+                    ),
+                    value: "authenticate"
+                },
+                ...switch_to_offline_mode !== null ? [
+                    {
+                        label: await this.#flux_localization_api.translate(
+                            "Switch to offline mode",
+                            AUTHENTICATION_LOCALIZATION_MODULE
+                        ),
+                        value: "switch-to-offline-mode"
+                    }
+                ] : []
+            ]
+        );
+
+        flux_overlay_element.style.setProperty("--flux-overlay-z-index", 1000);
+
+        flux_overlay_element.addEventListener(FLUX_OVERLAY_BUTTON_CLICK_EVENT, e => {
+            flux_overlay_element.buttons = flux_overlay_element.buttons.map(button => ({
+                ...button,
+                disabled: true
+            }));
+
+            flux_overlay_element.loading = true;
+
+            switch (e.detail.value) {
+                case "authenticate":
+                    authenticate();
+                    break;
+
+                case "switch-to-offline-mode":
+                    switch_to_offline_mode();
+                    break;
+
+                default:
+                    break;
+            }
+        });
+
+        document.body.appendChild(flux_overlay_element);
+
+        set_hide_authentication(
+            () => {
+                flux_overlay_element.buttons = flux_overlay_element.buttons.map(button => ({
+                    ...button,
+                    disabled: false
+                }));
+
+                flux_overlay_element.loading = false;
+            },
+            () => {
+                flux_overlay_element.remove();
+
+                resolve_promise();
+            }
+        );
+
+        return promise;
     }
 }
